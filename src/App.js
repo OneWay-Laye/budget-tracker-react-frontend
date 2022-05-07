b3c9e6aa60036659c508a684d87ffa772883d877
OneWay-Laye <tayloralaye@gmail.com>
2022-05-07 02:55:12 -0400

    This is the initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route } from 'react-router-dom'
+import { v4 as uuid } from 'uuid'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <label>Enter Email</label>
+        <input type='email'/>
+        <label>Enter Password</label>
+        <input type='password'/>
+        <label>Enter Password Again</label>
+        <input type='password'/>
+      </form>
     </div>
   );
 }
---
+++ src/components/SignUp/SignUp.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+class SignUp extends Component {
+  constructor (porps) {
+    super(porps)
+
+    this.state = {
+      email: '',
+      password: '',
+      password_confirmation: ''
+    }
+  }
+
+handleChange = event => this.setState({
+  [event.target.name]: event.target.value
+})
+
+onSignUp = event => {
+  event.preventDefault()
+
+  const 
+}
+}
